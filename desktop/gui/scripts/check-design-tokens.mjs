1e0099f03895c60d89403dbfaa5e24dd4333589f
unicbm <icccbm@outlook.com>
2026-08-10 23:24:41 +0800

    Finalize DemoTracer v1.1.0 release

--- desktop/gui/scripts/check-design-tokens.mjs
+++ desktop/gui/scripts/check-design-tokens.mjs
@@ -1,3 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) 2026 unicbm. All rights reserved.
+ * Licensed under the GNU Affero General Public License v3.0 only.
+ * See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { readdirSync, readFileSync } from "node:fs";
 import { basename, extname, join, resolve } from "node:path";
 
